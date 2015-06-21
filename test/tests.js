48281e0e018574b9f3721c3a9fce9e37005456df
Eric Eldredge <lettertwo@gmail.com>
2015-06-21 11:37:48 -0400

    Use async/await in tests
    Goodbye callback hell!

--- test/tests.js
+++ test/tests.js
@@ -6,7 +6,6 @@ import React from 'react/addons';
 const {assert} = chai;
 const {TestUtils} = React.addons;
 const nocache = (url) => `${url}?r=${Math.floor(Math.random() * Date.now() / 16)}`;
-function defer(fn) { setTimeout(fn, 0); }
 
 
 describe('ReactImageLoader', () => {
@@ -38,62 +37,34 @@ describe('ReactImageLoader', () => {
     assert.equal(loader.getDOMNode().childElementCount, 0, 'Expected wrapper to have no children');
   });
 
-  it('renders the image as visible when load completes', (done) => {
-    const loader = TestUtils.renderIntoDocument(
-      <ImageLoader
-        src={nocache('tiger.svg')}
-        onLoad={() => {
-          // FIXME: We set a timeout here because the style change we're testing
-          // happens on the next render. Is there a cleaner, less brittle way to
-          // test this?
-          defer(() => {
-            const img = TestUtils.findRenderedDOMComponentWithTag(loader, 'img');
-            assert.notOk(img.props.style.display, 'Expected img display to be falsy');
-            done();
-          });
-        }} />
-    );
+  it('renders the image when load completes', async function() {
+    const loader = await loadImage({src: nocache('tiger.svg')});
+    assert(TestUtils.findRenderedDOMComponentWithTag(loader, 'img'));
   });
 
-  it('shows alternative string on error', (done) => {
-    const loader = TestUtils.renderIntoDocument(
-      <ImageLoader
-        src={nocache('fake.jpg')}
-        onError={() => {
-          // FIXME: We set a timeout here because the style change we're testing
-          // happens on the next render. Is there a cleaner, less brittle way to
-          // test this?
-          defer(() => {
-            const span = TestUtils.findRenderedDOMComponentWithTag(loader, 'span');
-            const textNodes = TestUtils.findAllInRenderedTree(span, (n) => TestUtils.isTextComponent(n));
-            assert.lengthOf(textNodes, 1, 'Expected one text node');
-            assert.equal(textNodes[0].props, 'error', 'Expected error message to be rendered');
-            done();
-          });
-        }}>
-        error
-      </ImageLoader>
-    );
+  it('shows alternative string on error', async function() {
+    try {
+      await loadImage({src: nocache('fake.jpg')}, 'error');
+    } catch (loader) {
+      const span = TestUtils.findRenderedDOMComponentWithTag(loader, 'span');
+      const textNodes = TestUtils.findAllInRenderedTree(span, (n) => TestUtils.isTextComponent(n));
+      assert.lengthOf(textNodes, 1, 'Expected one text node');
+      assert.equal(textNodes[0].props, 'error', 'Expected error message to be rendered');
+      return;
+    }
+    throw new Error('Load should have failed!');
   });
 
-  it('shows alternative img node on error', (done) => {
-    const loader = TestUtils.renderIntoDocument(
-      <ImageLoader
-        src={nocache('fake.jpg')}
-        onError={() => {
-          // FIXME: We set a timeout here because the style change we're testing
-          // happens on the next render. Is there a cleaner, less brittle way to
-          // test this?
-          defer(() => {
-            const span = TestUtils.findRenderedDOMComponentWithTag(loader, 'span');
-            const imgNodes = TestUtils.scryRenderedDOMComponentsWithTag(span, 'img');
-            assert.lengthOf(imgNodes, 2, 'Expected two img node');
-            done();
-          });
-        }}>
-        <img />
-      </ImageLoader>
-    );
+  it('shows alternative img node on error', async function() {
+    try {
+      await loadImage({src: nocache('fake.jpg')}, <img />);
+    } catch (loader) {
+      const span = TestUtils.findRenderedDOMComponentWithTag(loader, 'span');
+      const imgNodes = TestUtils.scryRenderedDOMComponentsWithTag(span, 'img');
+      assert.lengthOf(imgNodes, 2, 'Expected two img node');
+      return;
+    }
+    throw new Error('Load should have failed!');
   });
 
   it('shows a preloader if provided', () => {
@@ -101,50 +72,40 @@ describe('ReactImageLoader', () => {
     assert(TestUtils.findRenderedDOMComponentWithTag(loader, 'div'));
   });
 
-  it('removes a preloader when load completes', (done) => {
-    const loader = TestUtils.renderIntoDocument(
-      <ImageLoader
-        src={nocache('tiger.svg')}
-        preloader={React.DOM.div}
-        onLoad={() => {
-          // FIXME: We set a timeout here because the style change we're testing
-          // happens on the next render. Is there a cleaner, less brittle way to
-          // test this?
-          defer(() => {
-            assert.throws(() => { TestUtils.findRenderedDOMComponentWithTag(loader, 'div'); });
-            done();
-          });
-        }} />
-    );
+  it('removes a preloader when load completes', async function() {
+    const loader = await loadImage({src: nocache('tiger.svg'), preloader: React.DOM.div});
+    assert.throws(() => { TestUtils.findRenderedDOMComponentWithTag(loader, 'div'); });
   });
 
-  it('removes a preloader when load fails', (done) => {
-    const loader = TestUtils.renderIntoDocument(
-      <ImageLoader
-        src={nocache('fake.jpg')}
-        preloader={React.DOM.div}
-        onError={() => {
-          // FIXME: We set a timeout here because the style change we're testing
-          // happens on the next render. Is there a cleaner, less brittle way to
-          // test this?
-          defer(() => {
-            assert.throws(() => { TestUtils.findRenderedDOMComponentWithTag(loader, 'div'); });
-            done();
-          });
-        }} />
-    );
+  it('removes a preloader when load fails', async function() {
+    try {
+      await loadImage({src: nocache('fake.jpg'), preloader: React.DOM.div});
+    } catch (loader) {
+      assert.throws(() => { TestUtils.findRenderedDOMComponentWithTag(loader, 'div'); });
+      return;
+    }
+    throw new Error('Load should have failed!');
   });
 
-  it('transfers img props to the underlying img element', () => {
-    const loader = TestUtils.renderIntoDocument(
-      <ImageLoader
-        src={nocache('tiger.svg')}
-        alt="this is alt text"
-        style={{width: 100}} />
-    );
+  it('transfers img props to the underlying img element', async function() {
+    const loader = await loadImage({src: nocache('tiger.svg'), alt: 'this is alt text', style: {width: 100}});
     const img = TestUtils.findRenderedDOMComponentWithTag(loader, 'img');
     assert.equal(img.props.style.width, 100, 'Expected img width to be 100');
     assert.equal(img.props.alt, 'this is alt text', 'Expected img to have alt text');
   });
 
 });
+
+
+function loadImage(props, children) {
+  return new Promise((resolve, reject) => {
+    const loader = TestUtils.renderIntoDocument(
+      <ImageLoader
+        {...props}
+        onLoad={() => { resolve(loader); }}
+        onError={() => { reject(loader); }}>
+        {children}
+      </ImageLoader>
+    );
+  });
+}
