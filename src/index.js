f6f139689f7c187e3b81dd14f8eb85b55135fd87
Eric Eldredge <lettertwo@gmail.com>
2015-06-21 11:41:18 -0400

    Implement the basic loading mechanism

--- src/index.js
+++ src/index.js
@@ -26,14 +26,58 @@ export default React.createClass({
     return {wrapper: span};
   },
 
+  componentDidMount() {
+    if (this.state.status === Status.LOADING) {
+      this.createLoader();
+    }
+  },
+
   getClassName() {
     let className = `imageloader ${this.state.status}`;
     if (this.props.className) className = `${className} ${this.props.className}`;
     return className;
   },
 
+  createLoader() {
+    this.destroyLoader();  // We can only have one loader at a time.
+
+    this.img = new Image();
+    this.img.addEventListener('load', this.handleLoad);
+    this.img.addEventListener('error', this.handleError);
+    this.img.src = this.props.src;
+  },
+
+  destroyLoader() {
+    if (this.img) {
+      this.img.removeEventListener('load', this.handleLoad);
+      this.img.removeEventListener('error', this.handleError);
+      delete this.img;
+    }
+  },
+
+  handleLoad(event) {
+    this.destroyLoader();
+    this.setState({status: Status.LOADED});
+
+    if (this.props.onLoad) this.props.onLoad(event);
+  },
+
+  handleError(error) {
+    this.destroyLoader();
+    this.setState({status: Status.FAILED});
+
+    if (this.props.onError) this.props.onError(error);
+  },
+
   render() {
-    const wrapperProps = [{className: this.getClassName()}];
+    let wrapperProps = [{className: this.getClassName()}];
+
+    switch (this.state.status) {
+      case Status.LOADED:
+        wrapperProps.push(<img src={this.props.src} />);
+        break;
+    }
+
     return this.props.wrapper(...wrapperProps);
   },
 });
